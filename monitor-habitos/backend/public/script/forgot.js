813e130ca45fc2eaf9c881413a4533a4744b8352
Bianca Viana <138331430+bvianas@users.noreply.github.com>
2025-11-22 18:24:05 -0300

    Update forgot.js

--- monitor-habitos/backend/public/script/forgot.js
+++ monitor-habitos/backend/public/script/forgot.js
@@ -8,9 +8,7 @@ document.getElementById("forgotForm").addEventListener("submit", function(e) {
     return;
   }
 
-  // Simulação do envio do link
   alert(`Um link de redefinição foi enviado para ${email} (simulação).`);
-  
-  // Redireciona para a tela de redefinição
-  window.location.href = "reset.html";
+
+  window.location.href = "script/reset.html";
 });
